554f4846cf0e5b7b0ebb2fa02dee5d0a2da06aab
ramesh <rameshkumarjayam@gmail.com>
2022-09-23 01:55:38 +0530

    okie

---
+++ html/validpassword.js
@@ -0,0 +1,8 @@
+function validpassword(){
+    var password=document.login.password.value;
+    var password
+    if (pawwword.lenght<6) {
+        alert("please enter 6 digit password");
+        return false;
+    }
+}
