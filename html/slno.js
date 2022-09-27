7e5eb152ea98da83550a33eaa76dc2ee842b9e61
ramesh <rameshkumarjayam@gmail.com>
2022-09-27 23:30:07 +0530

    okie

--- html/slno.js
+++
@@ -1,8 +0,0 @@
-const slno=[];
-slno.Put(1);
-console.log(slno)
-const newSlno=slno.map(num=>num+1);
-console.log(newSlno);
-return newSlno;
-//slno.push(newSlno);
-//document.getElementById(newSlno).innerHTML=newSlno;
