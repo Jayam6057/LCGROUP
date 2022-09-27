f3024c8fd3e05fdbc375ce22ba87b5e6a2d337bc
ramesh <rameshkumarjayam@gmail.com>
2022-09-27 12:20:40 +0530

    koiee

---
+++ html/slno.js
@@ -0,0 +1,8 @@
+const slno=[];
+slno.Put(1);
+console.log(slno)
+const newSlno=slno.map(num=>num+1);
+console.log(newSlno);
+return newSlno;
+//slno.push(newSlno);
+//document.getElementById(newSlno).innerHTML=newSlno;
--- html/validpassword.js
+++
@@ -1,8 +0,0 @@
-function validpassword(){
-    var password=document.getElementById("password");
-    if (password==="text") {
-        alert("please enter valid password");
-        return false;
-        redireto
-    }
-}
