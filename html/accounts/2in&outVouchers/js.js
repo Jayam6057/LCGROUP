115b3c021ee409e67bbfcf21af2e6ee0d99cb521
ramesh <rameshkumarjayam@gmail.com>
2022-10-06 23:24:52 +0530

    okiee

---
+++ html/accounts/2in&outVouchers/js.js
@@ -0,0 +1,11 @@
+function addRowCount(tableA) {
+    $(tableA).each(function(){
+      $('th:first-child, thead td:first-child', this).each(function(){
+        var tag = $(this).prop('tagName');
+        $(this).before('<'+tag+'>#</'+tag+'>');
+      });
+      $('td:first-child', this).each(function(i){
+        $(this).before('<td>'+(i+1)+'</td>');
+      });
+    });
+  }
