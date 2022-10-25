1c49901475bc4cb439c6d28b8a8ecd94d919fa92
ramesh <rameshkumarjayam@gmail.com>
2022-10-25 12:22:41 +0530

    okiee

---
+++ html/accounts/date.js
@@ -0,0 +1,3 @@
+const h=new Date("DD-MM-YYYY");
+document.getElementById("dt").innerHTML=h;
+console.log("dt");
---
+++ html/assets/slno.js
@@ -0,0 +1,6 @@
+var i="";
+for (var i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+    console.log("i");
